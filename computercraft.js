d30a0b945a5cc752b667876e730f7e26f6d8311f
Jason <1lanncontact@gmail.com>
2014-01-27 20:47:48 +0800

    Initial commit

---
+++ computercraft.js
@@ -0,0 +1,226 @@
+String.prototype.repeat = function( num )
+{
+    return new Array( num + 1 ).join( this );
+}
+
+// Shared in rendering
+var cursorPos;
+var textColor;
+var cursorBlink;
+
+var callLua;
+
+var computercraft = function() {
+	var C = Lua5_1.C;
+
+	var L = C.lua_open();
+	C.luaL_openlibs(L);
+
+	// Term variables
+	cursorPos = [1,1];
+	textColor = "#FFF";
+	cursorBlink = false;
+	var bgColor = "#000";
+	var width = 51;
+	var height = 19;
+
+	// OS variables
+	var osVersion = "CraftOS 1.5 (Web Alpha)";
+	var tempID = 1;
+	var label = null;
+	var startClock = null;
+
+	// Coroutines and events!
+	var eventStack = [];
+	var latestID = 0;
+
+	var functions = {
+		"term": {
+			"write": function(L) {
+				var str = C.luaL_checkstring(L, 1);
+				drawText(cursorPos[0],cursorPos[1],str,textColor,bgColor);
+				cursorPos[0] += str.length;
+				if (!blinkState) {
+					oCtxt.clearRect(0,0,canvas.width,canvas.height);
+					oCtxt.fillStyle = textColor;
+					oCtxt.fillText("_",((cursorPos[0]-1)*textWidth)+5,((cursorPos[1]-1)*textHeight)+18);
+				}
+				return 0;
+			},
+			"clear": function(L) {
+				for (i = 1; i <= height; i++) {
+					drawText(1,i," ".repeat(width),"#000",bgColor);
+				}
+			},
+			"clearLine": function(L) {
+				drawText(1,cursorPos[1]," ".repeat(width),"#000",bgColor);
+			},
+			"setCursorPos": function(L) {
+				var x = C.luaL_checkint(L, 1);
+				var y = C.luaL_checkint(L, 2);
+				cursorPos = [x,y];
+				if (!blinkState) {
+					oCtxt.clearRect(0,0,canvas.width,canvas.height);
+					oCtxt.fillStyle = textColor;
+					oCtxt.fillText("_",((x-1)*textWidth)+5,((y-1)*textHeight)+18);
+				}
+			},
+			"getCursorPos": function(L) {
+				C.lua_pushnumber(cursorPos[0]);
+				C.lua_pushnumber(cursorPos[1]);
+				return 1, 2;
+			},
+			"setCursorBlink": function(L) {
+				if(C.lua_isboolean(L, 1)){
+					cursorBlink = C.lua_toboolean(L, 1);
+					if (!cursorBlink) {
+						oCtxt.clearRect(0,0,620,350);
+						blinkState = false;
+					}
+					return 0;
+				} else {
+					C.lua_pushstring(L, "Expected boolean");
+					C.lua_error(L);
+				}
+			},
+			"setTextColor": function(L) {
+				// Not properly supported
+				var color = C.luaL_checkstring(L, 1);
+				textColor = color;
+				return 0;
+			},
+			"setBackgroundColor": function(L) {
+				// Not properly supported
+				var color = C.luaL_checkstring(L, 1);
+				bgColor = color;
+				return 0;
+			},
+			"isColor": function(L) {
+				// Black and white: Coming soon!
+				C.lua_pushboolean(L, 1);
+				return 1;
+			},
+			"getSize": function(L) {
+				C.lua_pushnumber(L, width);
+				C.lua_pushnumber(L, height);
+				return 1, 2;
+			},
+			"scroll": function(L) {
+				var amount = C.luaL_checkint(L, 1);
+				// Extra rendering crap is handled here!
+				var imgd = ctxt.getImageData(4, 4, canvas.width-8, canvas.height-8);
+				ctxt.clearRect(0,0,canvas.width,canvas.height);
+				ctxt.putImageData(imgd, 4, textHeight*amount*(-1)+4);
+			},
+			"redirect": function(L) {
+				// Function not supported
+				C.lua_pushstring(L, "redirect is not supported at this time!");
+				C.lua_error(L);
+			},
+			"restore": function(L) {
+				// Function not supported
+				C.lua_pushstring(L, "restore is not supported at this time!");
+				C.lua_error(L);
+			}
+		},
+		"os": {
+			"getComputerID": function(L) {
+				C.lua_pushnumber(L, tempID);
+				return 1;
+			},
+			"getComputerLabel": function(L) {
+				if (label) {
+					C.lua_pushstring(L, label);
+					return 1;
+				} else {
+					return 0;
+				}
+			},
+			"setComputerLabel": function(L) {
+				var str = C.luaL_checkstring(L, 1);
+				label = str;
+				return 0;
+			},
+			"clock": function(L) {
+				var diff = Date.now() - startClock;
+				var retDiff = Math.round(diff*0.1)/100;
+				C.lua_pushnumber(L, retDiff);
+				return 1;
+			},
+			"time": function(L) {
+				C.lua_pushstring(L, "Time not supported!");
+				C.lua_error(L);
+			},
+			"startTimer": function(L) {
+				var time = C.luaL_checknumber(L, 1);
+				latestID++;
+				var timerID = latestID;
+				setTimeout(function() { eventStack.push(["timer",latestID]) }, time*1000)
+				C.lua_pushnumber(timerID);
+				return 1;
+			},
+			"queueEvent": function(L) {
+				var queueObject = [];
+				queueObject.push(C.luaL_checkstring(L, 1));
+				var top = lua_gettop(L);
+				for (i = 1; i <= top; i++) {
+					var t = lua_type(L,i);
+					if (t == C.LUA_TSTRING) {
+						queueObject.push(C.lua_tostring(L,i));
+					} else if (t == C.LUA_TBOOLEAN) {
+						if C.lua_toboolean(L,i) {
+							queueObject.push(true);
+						} else {
+							queueObject.push(false);
+						}
+					} else if (t == C.LUA_TNUMBER) {
+						queueObject.push(C.lua_tonumber(L,i));
+					} else {
+						queueObject.push(null);
+					}
+				}
+				eventStack.push(queueObject);
+			},
+			"shutdown": function(L) {
+
+			},
+			"reboot": function(L) {
+
+			}
+		}
+	}
+
+	functions["os"]["computerLabel"] = functions["os"]["getComputerLabel"];
+
+	for (api in functions) {
+		if (typeof(functions[api]) == "function") {
+			C.lua_pushcfunction(
+				L,
+				Lua5_1.Runtime.addFunction(
+					functions[api]
+					)
+				);
+			C.lua_setfield(L, 1, api)
+		} else {
+			C.lua_newtable(L);
+			for (key in functions[api]) {
+				C.lua_pushcfunction(
+					L,
+					Lua5_1.Runtime.addFunction(
+						functions[api][key]
+						)
+					);
+				C.lua_setfield(L, 1, key)
+			}
+			C.lua_setglobal(L, api);
+		}
+	}
+
+	callLua = function(data) {
+		C.luaL_dostring(L,data);
+	}
+
+	
+	// Initiate clock 0
+	startClock = Date.now();
+};
---
+++ render.js
@@ -0,0 +1,87 @@
+var textWidth = 12;
+var textHeight = 18;
+
+var canvas = document.getElementById("canvas");
+var overlayCanvas = document.getElementById("overlayCanvas");
+var ctxt = canvas.getContext("2d");
+var oCtxt = overlayCanvas.getContext("2d")
+
+ctxt.font = "16px 'minecraftia'"
+oCtxt.font = "16px 'minecraftia'"
+var drawText;
+var drawChar;
+
+var blinkState = true;
+
+renderMain = function() {
+	drawChar = function(x,y,char,color,bg) {
+		if (!((x < 1) || (y < 1))) {
+			var spacing = {",":3, "l":3, ".":3, "i":5, "t":3,
+				"f":2, "I":2, "!":5}
+			var offset = 0;
+			if (spacing[char]) {
+				offset = spacing[char];
+			}
+			ctxt.beginPath();
+			ctxt.rect(((x-1)*textWidth+4),((y-1)*textHeight+4),textWidth,textHeight)
+			ctxt.fillStyle = bg;
+			ctxt.fill();
+			ctxt.fillStyle = color;
+			ctxt.fillText(char,((x-1)*textWidth)+5+offset,((y-1)*textHeight)+18);
+		}
+	}
+	drawText = function(x,y,text,textColor,bgColor) {
+		for (var i = 0; i <= text.length-1; i++) {
+			drawChar(x+i,y,text.charAt(i),textColor,bgColor)
+		}
+	}
+	cursorBlinking = function() {
+		if (cursorBlink && blinkState) {
+			if (blinkState) {
+				oCtxt.fillStyle = textColor;
+				oCtxt.fillText("_",((cursorPos[0]-1)*textWidth)+5,((cursorPos[1]-1)*textHeight)+18);
+				blinkState = false;
+			}
+		} else {
+			oCtxt.clearRect(0,0,canvas.width,canvas.height);
+			blinkState = true;
+		}
+	}
+	setInterval(cursorBlinking,500)
+
+	// for (var x = 0; x <= 51; x++) {
+	// 	ctxt.beginPath();
+	// 	ctxt.moveTo(x*textWidth+4,4);
+	// 	ctxt.lineTo(x*textWidth+4,350);
+	// 	ctxt.strokeStyle = "#FFF";
+	// 	ctxt.stroke();
+	// }
+	// for (var y = 0; y <= 19; y++) {
+	// 	ctxt.beginPath();
+	// 	ctxt.moveTo(4,y*textHeight+4);
+	// 	ctxt.lineTo(620,y*textHeight+4);
+	// 	ctxt.strokeStyle = "#FFF";
+	// 	ctxt.stroke();
+	// }
+}
+
+var link = document.createElement('link');
+var loaded = false;
+
+// Trick from http://stackoverflow.com/questions/2635814/
+var image = new Image;
+image.src = "minecraftia-webfont.ttf";
+image.onerror = function() {
+	setTimeout(function() {
+		loaded = true;
+		document.getElementById("loading").style.display = "none";
+		renderMain();
+		if (typeof(computercraft) != "undefined") {
+			computercraft();
+		} else {
+			drawText(1,1,"An error occured while loading ComputerCraft!","#CC4C4C","#000");
+			drawText(1,2,"Check the JavaScript console for more details","#CC4C4C","#000");
+		}
+	}, 100);
+}
+
