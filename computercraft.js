5a46d5e4d648f27f79cda8298a60a9f6478e2ea3
Jason <1lanncontact@gmail.com>
2014-02-21 20:58:40 +0800

    Tonnes of updates, fixes, and everything!

--- computercraft.js
+++ computercraft.js
@@ -1,6 +1,6 @@
 String.prototype.repeat = function( num )
 {
-    return new Array( num + 1 ).join( this );
+	return new Array( num + 1 ).join( this );
 }
 
 // Shared in rendering
@@ -15,6 +15,8 @@ var computercraft = function() {
 
 	var L = C.lua_open();
 	C.luaL_openlibs(L);
+
+	var resumeThread;
 
 	// Term variables
 	cursorPos = [1,1];
@@ -155,7 +157,7 @@ var computercraft = function() {
 				var time = C.luaL_checknumber(L, 1);
 				latestID++;
 				var timerID = latestID;
-				setTimeout(function() { eventStack.push(["timer",latestID]) }, time*1000)
+				setTimeout(function() { eventStack.push(["timer",latestID]); resumeThread(); }, time*1000);
 				C.lua_pushnumber(timerID);
 				return 1;
 			},
@@ -168,7 +170,7 @@ var computercraft = function() {
 					if (t == C.LUA_TSTRING) {
 						queueObject.push(C.lua_tostring(L,i));
 					} else if (t == C.LUA_TBOOLEAN) {
-						if C.lua_toboolean(L,i) {
+						if (C.lua_toboolean(L,i)) {
 							queueObject.push(true);
 						} else {
 							queueObject.push(false);
@@ -219,8 +221,103 @@ var computercraft = function() {
 	callLua = function(data) {
 		C.luaL_dostring(L,data);
 	}
-
 	
 	// Initiate clock 0
 	startClock = Date.now();
+
+	var mainThread = C.lua_newthread(L);
+	C.luaL_loadstring(mainThread, "term.write('hi') term.write(' '..coroutine.yield()) coroutine.yield() term.write(' passed!')");
+
+	var threadAlive = true;
+	var threadLoop;
+	resumeThread = function() {
+		if (threadAlive) {
+			threadLoop = setInterval(function () {
+				if (eventStack.length > 0) {
+					var argumentsNumber = eventStack[0].length;
+
+					for (var index in eventStack[0]) {
+						C.lua_pushstring(mainThread,""+eventStack[0][index]);
+					}
+					eventStack.splice(0,1)
+
+					var resp = C.lua_resume(mainThread,argumentsNumber);
+					if (resp == C.LUA_YIELD) {
+
+					} else if (resp == 0) {
+						clearInterval(threadLoop);
+						trheadAlive = false;
+						console.log("Program complete")
+						console.log("Thread closed")
+					} else {
+						console.log("Error: "+C.lua_tostring(mainThread,-1));
+						clearInterval(threadLoop);
+						threadALive = false;
+						console.log("Thread closed")
+					}
+				} else {
+					clearInterval(threadLoop);
+					console.log("Thread suspended")
+				}
+			}, 10);
+		}
+	}
+	
+	var initialization = function() {
+		var resp = C.lua_resume(mainThread,0);
+		if ((resp != C.LUA_YIELD) && (resp != 0)) {
+			var errorCode = C.lua_tostring(mainThread,-1);
+			var trace = C.lua_tostring(mainThread,-3);
+			console.log("Intialization Error: "+errorCode);
+			threadAlive = false;
+			console.log("Thread closed")
+			for (i = 1; i <= height; i++) {
+				drawText(1,i," ".repeat(width),"#000","#0000AA");
+			}
+			drawText(13,7,"WEBCC : FATAL : BIOS ERROR","#0000AA","#FFF");
+			var startPos = Math.round(width/2 - ((7+errorCode.length)/2));
+			drawText(startPos,9,"ERROR: "+errorCode,"#FFF","#0000AA");
+			if (trace) {
+				function wordWrap(str, maxWidth) {
+					var white = new RegExp(/^\s$/);
+					var newLineStr = "\n"; done = false; res = [];
+					do {                    
+						found = false;
+					// Inserts new line at first whitespace of the line
+					for (i = maxWidth - 1; i >= 0; i--) {
+						if (white.test(str.charAt(i))) {
+							res.push(str.slice(0, i));
+							str = str.slice(i + 1);
+							found = true;
+							break;
+						}
+					}
+					// Inserts new line at maxWidth position, the word is too long to wrap
+					if (!found) {
+						res.push(str.slice(0, maxWidth));
+						str = str.slice(maxWidth);
+					}
+
+					if (str.length < maxWidth)
+						done = true;
+					} while (!done);
+
+					return res;
+				}
+				var words = wordWrap(trace,40)
+				drawText(17,11,"-- MORE DETAILS --","#FFF","#0000AA");
+				var yIndex = 11;
+				for (var index in words) {
+					yIndex++;
+					var startPos = Math.round(width/2 - ((words[index].length)/2));
+					drawText(startPos,yIndex,words[index],"#FFF","#0000AA");
+				}
+			}
+		}
+	}
+	initialization();
+	eventStack.push(["test event"]);
+	resumeThread();
+	eventStack.push(["test event"]);
+	resumeThread();
 };
