45e7aed2c11a2fc7bacf9925d3571abfd7e6b01c
Ben Anderson <gravityscore@gmail.com>
2014-02-28 15:58:31 +0800

    Beginnings of FS API

--- scripts/apis/fs.js
+++ scripts/apis/fs.js
@@ -6,6 +6,7 @@
 
 
 
+var fs;
 var fsAPI = {};
 var filesystem = {};
 
@@ -24,6 +25,13 @@ triggerGUIUpdate = function() {
 
 
 filesystem.setup = function(callback) {
+	BrowserFS.install(window);
+
+	var lsfs = new BrowserFS.FileSystem.LocalStorage();
+	BrowserFS.initialize(lsfs);
+
+	fs = require("fs");
+
 	callback(null);
 }
 
@@ -121,54 +129,36 @@ filesystem.isReadOnly = function(path) {
 }
 
 
-filesystem.list = function(path, callback) {
-	
+filesystem.getName = function(path) {
+	return path.substring(path.lastIndexOf("/") + 1);
 }
 
 
-filesystem.allFiles = function(path, callback) {
-	
-}
-
-
-filesystem.move = function(from, to, callback) {
-	
-}
-
-
-filesystem.copy = function(from, to, callback) {
-	
-}
-
-
-filesystem.delete = function(path, callback) {
-	
-}
-
-
-filesystem.read = function(path, callback) {
-	
-}
-
-
-filesystem.write = function(path, contents, append, callback) {
-	
-}
-
-
-filesystem.makeDir = function(path, callback) {
-	
+filesystem.getContainingFolder = function(path) {
+	return path.substring(0, path.lastIndexOf("/"));
 }
 
 
 
-//  ----------------  Lua Wrappers  ----------------  //
+//  ----------------  Lua Functions  ----------------  //
 
 
 fsAPI.list = function(L) {
 	var path = filesystem.resolve(C.luaL_checkstring(L, 1));
+	var files = fs.readdirSync(path);
+	if (files) {
+		C.lua_newtable(L);
+		for (var i in files) {
+			C.lua_pushnumber(L, i + 1);
+			C.lua_pushstring(L, files[i]);
+			C.lua_rawset(L, -3);
+		}
 
-	return 1;
+		return 1;
+	} else {
+		console.log("fs.list error", files);
+		return 0;
+	}
 }
 
 
@@ -180,6 +170,8 @@ fsAPI.getSize = function(L) {
 
 fsAPI.exists = function(L) {
 	var path = filesystem.resolve(C.luaL_checkstring(L, 1));
+	var exists = fs.existsSync(path);
+	C.lua_pushnumber(L, exists ? 1 : 0);
 
 	return 1;
 }
@@ -187,6 +179,12 @@ fsAPI.exists = function(L) {
 
 fsAPI.isDir = function(L) {
 	var path = filesystem.resolve(C.luaL_checkstring(L, 1));
+	var stat = fs.statSync(path);
+	if (stat.isDirectory()) {
+		C.lua_pushnumber(L, 1);
+	} else {
+		C.lua_pushnumber(L, 0);
+	}
 
 	return 1;
 }
@@ -196,54 +194,92 @@ fsAPI.isReadOnly = function(L) {
 	var path = filesystem.resolve(C.luaL_checkstring(L, 1));
 	var isReadOnly = filesystem.isReadOnly(path);
 	C.lua_pushnumber(L, isReadOnly ? 1 : 0);
+
 	return 1;
 }
 
 
 fsAPI.makeDir = function(L) {
 	var path = filesystem.resolve(C.luaL_checkstring(L, 1));
-	return 1;
+	fs.mkdir(path);
+
+	return 0;
 }
 
 
 fsAPI.move = function(L) {
 	var from = filesystem.resolve(C.luaL_checkstring(L, 1));
 	var to = filesystem.resolve(C.luaL_checkstring(L, 2));
-	return 1;
+	var toStat = fs.statSync(to);
+	if (toStat.isDirectory()) {
+		fs.renameSync(from, to + filesystem.getName(from));
+	} else if (!toStat.isFile()) {
+		fs.renameSync(from, to);
+	}
+
+	return 0;
 }
 
 
 fsAPI.copy = function(L) {
 	var from = filesystem.resolve(C.luaL_checkstring(L, 1));
 	var to = filesystem.resolve(C.luaL_checkstring(L, 2));
-	return 1;
+	var toStat = fs.statSync(to);
+	if (toStat.isDirectory()) {
+		fs.createReadStream(from).pipe(fs.createWriteStream(to + filesystem.getName(from)));
+	} else if (!toStat.isFile()) {
+		fs.createReadStream(from).pipe(fs.createWriteStream(to));
+	}
+
+	return 0;
 }
 
 
 fsAPI.delete = function(L) {
 	var path = filesystem.resolve(C.luaL_checkstring(L, 1));
-	return 1;
+	if (path != "/" + computer.id) {
+		fs.unlinkSync(path);
+	}
+
+	return 0;
 }
 
 
 fsAPI.write = function(L) {
 	var path = filesystem.resolve(C.luaL_checkstring(L, 1));
 	var contents = C.luaL_checkstring(L, 2);
-	return 1;
+	var stat = fs.statSync(path);
+	if (!stat.isDirectory()) {
+		fs.writeFileSync(path, contents);
+	}
+
+	return 0;
 }
 
 
 fsAPI.append = function(L) {
 	var path = filesystem.resolve(C.luaL_checkstring(L, 1));
 	var contents = C.luaL_checkstring(L, 2);
-	return 1;
+	var stat = fs.statSync(path);
+	if (!stat.isDirectory()) {
+		fs.appendFileSync(path, contents);
+	}
+
+	return 0;
 }
 
 
 fsAPI.read = function(L) {
 	var path = filesystem.resolve(C.luaL_checkstring(L, 1));
-	
-	return 1;
+	var stat = fs.statSync(path);
+	if (stat.isFile()) {
+		var contents = fs.readFileSync(path);
+		C.lua_pushstring(L, contents.toString());
+		
+		return 1;
+	} else {
+		return 0;
+	}
 }
 
 
